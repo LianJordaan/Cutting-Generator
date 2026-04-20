f814660d216a6b2e9e6ee95f86068c1c086a525b
Lian Jordaan <lianjordaan30052005@gmail.com>
2026-04-20 10:28:19 +0200

    Preparing to add new update for v4.5.3

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ from shape_gen import *
 print("Libraries loaded.")
 
 APP_NAME = "Cutting Generator"
-APP_VERSION = "v4.5.2"
+APP_VERSION = "v4.5.3"
 AUTHOR = "Lian Jordaan"
 
 WINDOW_TITLE = f"{APP_NAME} {APP_VERSION} - {AUTHOR}"
